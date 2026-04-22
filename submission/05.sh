ded93fe2c899d08a655c14e85d248f30750c4d61
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-22 11:44:47 +0100

    First part submission fix

--- submission/05.sh
+++ submission/05.sh
@@ -8,10 +8,10 @@ decodedtx=$(bitcoin-cli -signet decoderawtransaction $rawtx)
 prev_txid=$(echo $decodedtx | jq -r '.vin[0].txid')
 prev_vout=$(echo $decodedtx | jq -r '.vin[0].vout') 
 
-in_btc=$(bitcoin-cli -signet getrawtransaction "$prev_txid" 1 | jq -r ".vout[$prev_vout].value")
+input_btc=$(bitcoin-cli -signet getrawtransaction "$prev_txid" 1 | jq -r ".vout[$prev_vout].value")
 
-out_btc=$(echo $decodedtx | jq -r '[.vout[].value] | add')
+output_btc=$(echo $decodedtx | jq -r '[.vout[].value] | add')
 
 
-fee_satoshi=$(awk -v in="$in_btc" -v out="$out_btc" 'BEGIN { printf "%.0f\n", (in-out)*100000000 }')
+fee_satoshi=$(awk -v in="$input_btc" -v out="$output_btc" 'BEGIN { printf "%.0f\n", (in-out)*100000000 }')
 echo "$fee_satoshi"
