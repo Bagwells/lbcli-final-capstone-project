f56868a03934a30f0bbc89266bc393d49a933f7b
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-22 11:25:57 +0100

    First part submission fix

--- submission/03.sh
+++ submission/03.sh
@@ -10,7 +10,7 @@ blockhash_2=$(bitcoin-cli -signet getblockhash $block_2)
 
 coinbasetx=$(bitcoin-cli -signet getblock $blockhash_1 1 | jq -r '.tx[0]')
 
-bitcoin-cli -signet getblock $blockhash_2 2 | jq -r --arg coinbasetx "$coinbasetx" '.tx[] | select(any(.vin[].txid == $coinbasetx)) | .txid'
+bitcoin-cli -signet getblock $blockhash_2 2 | jq -r --arg coinbasetx "$coinbasetx" '.tx[] | select(any(.vin[]?; .txid? == $coinbasetx)) | .txid'
 
 
 
